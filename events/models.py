77934d83affc0c6fb2035465ba2b05d66a76a7e9
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-26 17:23:52 +0530

    ticket_price

--- events/models.py
+++ events/models.py
@@ -6,6 +6,7 @@ class Event(models.Model):
     date = models.DateTimeField()
 
     created_at = models.DateTimeField(auto_now_add=True)
+    ticket_rate = models.IntegerField(blank=False,null=False,default=0)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):

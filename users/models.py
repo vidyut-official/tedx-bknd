1e79a6de51b01b6a5fb5a865b731b621229dd717
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-26 13:49:13 +0530

    user model fixed

--- users/models.py
+++ users/models.py
@@ -1,20 +1,38 @@
 from django.db import models
-
-
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
 class User(AbstractUser):
+
+    # Remove username completely
+    username = None
+
+    # Email will be login
+    email = models.EmailField(unique=True)
+
+    full_name = models.CharField(max_length=150)
+
     USER_TYPE = [
-        ("Amrita","amrita"),
-        ("Others","others")
-        
+        ("amrita", "Amrita"),
+        ("others", "Others"),
     ]
+
     USER_ROLE = [
-        ("Registration","resgistration"),
-        ("Finance","finance"),
-        ("Participant", "participant"),
+        ("registration", "Registration"),
+        ("finance", "Finance"),
+        ("participant", "Participant"),
     ]
-    username = models.CharField(max_length=100,unique=False,blank=False,null=False)
-    user_type = models.CharField(max_length=20,choices=USER_TYPE,default="others",blank=False)
-    user_role = models.CharField(max_length=20, choices=USER_ROLE,default="participant")
+
+    user_type = models.CharField(
+        max_length=20,
+        choices=USER_TYPE,
+        default="others",
+    )
+
+    user_role = models.CharField(
+        max_length=20,
+        choices=USER_ROLE,
+        default="participant",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]

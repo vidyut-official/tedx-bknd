9918d9701282117a411534395bff97f91ea307ba
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-27 14:14:30 +0530

    url changed

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views.get_all_events import EventListAPIView
 
 urlpatterns = [
-    path("events/", EventListAPIView.as_view(), name="event-list"),
+    path("all/", EventListAPIView.as_view(), name="event-list"),
 ]

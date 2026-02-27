275c7f147e6323d1e66a3d25b0caf54b19bb6ddd
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-27 11:54:55 +0530

    static files added

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,8 @@ AUTH_USER_MODEL = "users.User"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

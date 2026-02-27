ca77992fdbff9c7bce76d0b8e756a7b2d42f64f5
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-27 14:19:56 +0530

    remaining_seats added to fetch all events

--- events/views/get_all_events.py
+++ events/views/get_all_events.py
@@ -1,10 +1,28 @@
+from django.db.models import Count, F
+from django.db.models.functions import Coalesce
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Event
+from tickets.models import Ticket
 
 
 class EventListAPIView(APIView):
 
     def get(self, request):
-        events = Event.objects.all().values("id", "name","quantity","ticket_rate")
+
+        events = (
+            Event.objects
+            .annotate(
+                tickets_sold=Coalesce(Count("ticket"), 0),
+                remaining_seats=F("quantity") - Coalesce(Count("ticket"), 0)
+            )
+            .values(
+                "id",
+                "name",
+                "quantity",
+                "ticket_rate",
+                "remaining_seats",
+            )
+        )
+
         return Response(list(events))
